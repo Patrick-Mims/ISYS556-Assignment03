1f9cd6d632907194fb22501bea0929f3178f0d84
Patrick Mims <patrickkevinmims@gmail.com>
2022-10-06 11:08:26 -0700

    Initial Commit

---
+++ ISYS556-Assignment03/ISYS556_Assignment03App.swift
@@ -0,0 +1,17 @@
+//
+//  ISYS556_Assignment03App.swift
+//  ISYS556-Assignment03
+//
+//  Created by JPL-ST-SPRING2022 on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct ISYS556_Assignment03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
