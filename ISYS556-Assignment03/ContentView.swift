9d1779f93784586afc58dd5939e1cec8541bdb20
Patrick Mims <patrickkevinmims@gmail.com>
2022-10-06 13:03:37 -0700

    Updated the copy and the icon. The images doesn't change though

--- ISYS556-Assignment03/ContentView.swift
+++ ISYS556-Assignment03/ContentView.swift
@@ -1,16 +1,27 @@
-//
-//  ContentView.swift
-//  ISYS556-Assignment03
-//
-//  Created by JPL-ST-SPRING2022 on 10/6/22.
-//
+/*
+ Name: Patrick Mims
+ Date: 10.06.22
+ Assignment: Weather Forecast with Button Spec
+ ISYS-556
+ */
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var msgStr = "Rain"
+    @State private var fctButton = "Tomorrow's Forecast"
+    @State private var icon = "cloud.heavyrain.fill"
     var body: some View {
-        Text("Hello, world!")
+        Image(systemName: icon)
+        Text(msgStr)
+            .font(.largeTitle)
+            .fontWeight(.medium)
+            .foregroundColor(.red)
             .padding()
+        Button(fctButton) {
+            fctButton = "Yesterday's Forecast"
+            msgStr = "Sunny!"
+        }
     }
 }
 
